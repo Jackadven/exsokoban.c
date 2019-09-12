c00486e2ec4e82c8d9cc5cca3b9850c533a3167c
Jefferson Queiroz Da Costa <jeffersonadven7@gmail.com>
2019-09-12 09:25:10 -0300

    adicionando o ramo develop e o template

---
+++ ex10.c
@@ -0,0 +1,79 @@
+/***************************************************************************
+ *   ex10.c                                   Version 20190912.090014      *
+ *                                                                         *
+ *   programa que declare uma matriz 3D de strings para labirinto do jogo  *
+ *   SOKOBAN, com movimentacao nas teclas hjkl                             *
+ *   Copyright (C) 2019         by: Jefferson Queiroz Da Costai            *
+ *                                                                         *
+ *                                                                         *
+ ***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; version 2 of the License.               *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *   To contact the author, please write to:                               *
+ *   Jefferson Queiroz Da Costa                                            *
+ *   Email: jeffersonadven7@gmail.com                                      *
+ *   Webpage: http://beco.poli.br/username                                 *
+ *   Phone: (87) 99951-1215                                                *
+ ***************************************************************************/
+
+/*
+ * Instrucoes para compilar:
+ * $gcc ex10.c -o ex10.x -Wall -Wextra -g -O0 -DDEBUG=1
+ * opcoes extras: -ansi -pedantic-errors
+ */
+
+/* ---------------------------------------------------------------------- */
+/* includes */
+
+#include <stdio.h> /* Standard I/O functions */
+#include <stdlib.h> /* Miscellaneous functions (rand, malloc, srand)*/
+#include <string.h> /* Strings functions definitions */
+
+/* ---------------------------------------------------------------------- */
+/* prototypes */
+
+
+/* ---------------------------------------------------------------------- */
+/* Funcao: Imprir a matriz 3d e movimentar-se nela
+ * 
+ * Entrada: muvimetacao do usuario
+ *
+ * Saida: Printar a matriz 3d 
+ */
+int main(void)
+{
+    /* local declarations */
+    int i; /* general index */
+   /*char labirinto[1][7][7]=  /Criacao do labirinto/ 
+    {   
+        "####",
+        "#  #",
+        "#  ###",
+        "#HO  #",
+        "#  H #",
+        "#  ###",
+        "####"
+    }
+    */
+    
+    return EXIT_SUCCESS;
+}
+
+
+
+/* ---------------------------------------------------------------------- */
+/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
+/* Template by Dr. Beco <rcb at beco dot cc> Version 20160612.142044      */
+
